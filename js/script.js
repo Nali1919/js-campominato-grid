8c140ab657bcfcfeac536ca7eedb198fc3c1ba4a
Nali1919 <my@email.com>
2022-09-02 15:50:51 +0200

    creo struttura ed effettuo prime modifiche

---
+++ js/script.js
@@ -0,0 +1,17 @@
+console.log('Js is ok')
+
+const buttonInizio = document.getElementById('bottone-inizio')
+console.log(buttonInizio)
+
+buttonInizio.addEventListener('click', function () {
+    console.log('oplà')
+    // elemento griglia 
+    const griglia = document.getElementById('griglia')
+
+    griglia.innerHTML = ''
+
+    //creazione celle
+    
+
+
+})
